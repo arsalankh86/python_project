c2d5c747ad35f7139fddca08c1e6ee258752d06d
arsalan.khan <arsalan.khan@seekinvest.com>
2023-08-10 03:17:29 +0500

    comit 1

--- basic.py
+++ basic.py
@@ -1,3 +1,14 @@
 print("Hello World")
 if True:
     print("1if")
+x=4
+print(x)
+x=5
+y=5
+z=x+y
+print(z)
+del z
+z=14
+print(type(x))
+name = input("please enter your name:")
+print(f"your name is {name}")
