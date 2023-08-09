06df8bb5631caa4d17a10d99ca49b70bcbe59546
arsalan.khan <arsalan.khan@seekinvest.com>
2023-08-10 03:59:54 +0500

    Implement Create User API using POST

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def home():
     return "Home"
 
+@app.route("/get_user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "id":user_id,
+        "name":"abc"
+    }
+    return jsonify(user_data), 200
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
